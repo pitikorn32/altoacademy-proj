9bc709ec0f0c339138f8b3abff851ab90c5bc441
Pitikorn Khlaisamniang <121279047+pitikorn32@users.noreply.github.com>
2023-05-20 07:10:01 +0000

    change forecast_steps

--- week_03/lab_01/src/main.py
+++ week_03/lab_01/src/main.py
@@ -3,10 +3,12 @@ from lab_01.app import get_wu_forecast_hourly, str_format_wu
 from lab_01.helper import LineNotify
 from config import LINE_ACCESS_TOKEN
 
+FORECAST_STEPS = 3
+
 if __name__ == "__main__":
     linenotify = LineNotify(LINE_ACCESS_TOKEN=LINE_ACCESS_TOKEN)
     datetime_now = pendulum.now("Asia/Bangkok").to_datetime_string()
-    data_forecast = get_wu_forecast_hourly(forecast_steps=2)
+    data_forecast = get_wu_forecast_hourly(forecast_steps=FORECAST_STEPS)
 
     str_forecast = str_format_wu(data_forecast)
     str_forecast = "\n".join(str_forecast)
